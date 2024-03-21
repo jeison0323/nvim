45f4e738e9ea2dcf0a42135e9458a05d95acf30b
Jeison Velásquez <jeison@netuxtecnologia.com>
2024-03-21 11:04:36 -0500

    chore: add buffer change mapping and lazy-lock to gitignore

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,10 +1,13 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", ":w<CR>")
 vim.api.nvim_set_keymap('n', '<leader>ee', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>ev', ':Ex<CR>')
 vim.keymap.set("n", "<c-h>", "<c-w>h", { noremap = true, silent = true })
 vim.keymap.set("n", "<c-l>", "<c-w>l", { noremap = true, silent = true })
 vim.keymap.set("n", "<c-j>", "<c-w>j", { noremap = true, silent = true })
 vim.keymap.set("n", "<c-k>", "<c-w>k", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<Tab>", ":bnext<CR>", { noremap = true, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
