a016dec3cfce61a56746859b485d3d200d9886fc
Jeison Velásquez S. <velasquezjeison23@hotmail.com>
2024-03-20 23:36:44 -0500

    chore: add indent automatic reselect lines

--- lua/user/remap.lua
+++ lua/user/remap.lua
@@ -1,18 +1,21 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>w", ":w<CR>")
-vim.api.nvim_set_keymap('n', '<leader>ee', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
-vim.keymap.set("n", "<c-h>", "<c-w>h", {noremap = true, silent = true})
-vim.keymap.set("n", "<c-l>", "<c-w>l", {noremap = true, silent = true})
-vim.keymap.set("n", "<c-j>", "<c-w>j", {noremap = true, silent = true})
-vim.keymap.set("n", "<c-k>", "<c-w>k", {noremap = true, silent = true})
+vim.api.nvim_set_keymap('n', '<leader>ee', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+vim.keymap.set("n", "<c-h>", "<c-w>h", { noremap = true, silent = true })
+vim.keymap.set("n", "<c-l>", "<c-w>l", { noremap = true, silent = true })
+vim.keymap.set("n", "<c-j>", "<c-w>j", { noremap = true, silent = true })
+vim.keymap.set("n", "<c-k>", "<c-w>k", { noremap = true, silent = true })
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "<c-u>", "<c-u>zz")
 vim.keymap.set("n", "<c-d>", "<c-d>zz")
 vim.keymap.set("n", "n", " nzzzv")
 vim.keymap.set("n", "N", " Nzzzv")
+
+vim.api.nvim_set_keymap('x', '>', ':><CR>gv', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('x', '<', ':<<CR>gv', { noremap = true, silent = true })
 
 vim.keymap.set("x", "<leader>p", "\"_dp")
 
