530423aa1be04f1ca2aa1dd767ef028d8793cde5
Jeison Velásquez S. <velasquezjeison23@hotmail.com>
2024-03-20 23:08:22 -0500

    chore: add system validation for ToggleTerm plugin

--- lua/user/plugins/toggleterm.lua
+++ lua/user/plugins/toggleterm.lua
@@ -2,18 +2,21 @@ return {
     'akinsho/toggleterm.nvim',
     version = "*",
     config = function()
-        local powershell_options = {
-          shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
-          shellcmdflag = "-Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
-          shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
-          shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
-          shellquote = "",
-          shellxquote = "",
-        }
-        for option, value in pairs(powershell_options) do
-            vim.opt[option] = value
+        if vim.fn.has("win32") == 1 then
+            local powershell_options = {
+                shell = vim.fn.executable "pwsh" == 1 and "pwsh" or "powershell",
+                shellcmdflag =
+                "-Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;",
+                shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait",
+                shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode",
+                shellquote = "",
+                shellxquote = "",
+            }
+            for option, value in pairs(powershell_options) do
+                vim.opt[option] = value
+            end
         end
-        require("toggleterm").setup{
+        require("toggleterm").setup {
             open_mapping = [[<c-\>]],
             shell = vim.o.shell,
             close_mapping = [[<c-\>]],
