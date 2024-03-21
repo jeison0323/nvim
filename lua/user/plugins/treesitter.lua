77858746bb094f0df6af60ec888b396e29d220e1
Jeison Velásquez S. <velasquezjeison23@hotmail.com>
2024-03-20 21:11:42 -0500

    refactor: change packer to lazy

---
+++ lua/user/plugins/treesitter.lua
@@ -0,0 +1,14 @@
+return {
+    "nvim-treesitter/nvim-treesitter",
+    config = function()
+        require'nvim-treesitter.configs'.setup {
+            ensure_installed = { "javascript", "python", "c", "lua", "vim", "vimdoc", "query" },
+            sync_install = false,
+            auto_install = true,
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+        }
+    end
+}
